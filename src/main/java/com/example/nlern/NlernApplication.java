fe8ab60729b291a41c04f57af4a0a5cca4094751
Yurii Romaniv <arderchinsky@gmail.com>
2023-03-10 20:30:19 +0200

    empty

---
+++ src/main/java/com/example/nlern/NlernApplication.java
@@ -0,0 +1,13 @@
+package com.example.nlern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlernApplication.class, args);
+    }
+
+}
