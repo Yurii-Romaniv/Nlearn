8d884933bf9923fff79c128fe9fd9c127f70035d
Yurii Romaniv <arderchinsky@gmail.com>
2023-03-15 11:01:36 +0200

    added User, repository for User, and endpoint for viewing and creating it

---
+++ src/main/java/com/example/nlern/repos/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.nlern.repos;
+
+import com.example.nlern.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository  extends CrudRepository<User, Integer> {
+    List<User> findTop5ByOrderByIdDesc();
+}
