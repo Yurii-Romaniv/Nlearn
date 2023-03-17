c9fc65aae94359ee9aaa3d95c117393eeb651668
Yurii Romaniv <arderchinsky@gmail.com>
2023-03-17 22:58:35 +0200

    created teacher's page and start realisation of create_test page

--- src/main/java/com/example/nlern/repos/TestRepository.java
+++ src/main/java/com/example/nlern/repos/TestRepository.java
@@ -3,6 +3,9 @@ package com.example.nlern.repos;
 import com.example.nlern.models.Test;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TestRepository extends CrudRepository<Test, Integer> {
+    List<Test> findTop5ByAuthorIdOrderByIdDesc(int Author);//findTop5ByOrderByIdDesc();
 
 }
