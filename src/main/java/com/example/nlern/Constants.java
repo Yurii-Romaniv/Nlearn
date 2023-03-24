c11ff0a1038e1f9889e0b77660b8b8f98866a737
Yurii Romaniv <arderchinsky@gmail.com>
2023-03-25 01:07:59 +0200

    added react, created test edit/create page, changed models

--- src/main/java/com/example/nlern/Constants.java
+++ src/main/java/com/example/nlern/Constants.java
@@ -1,4 +1,4 @@
-package com.example.nlern.models;
+package com.example.nlern;
 
 public class Constants {
     public static final int NUMBER_OF_QUESTIONS = 10;
--- src/main/java/com/example/nlern/models/FullTest.java
+++ src/main/java/com/example/nlern/models/FullTest.java
@@ -1,2 +1,46 @@
-package com.example.nlern.models;public class FullTest {
+package com.example.nlern.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FullTest {
+    Optional<Test>  test;
+    List<Question> questions;
+
+    List<Integer> deleted;
+    List<Integer> added ;
+
+    public Optional<Test>  getTest() {
+        return test;
+    }
+
+    public void setTest(Optional<Test> test) {
+        this.test = test;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+
+    public List<Integer> getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(ArrayList<Integer> deleted) {
+        this.deleted = deleted;
+    }
+
+    public List<Integer> getAdded() {
+        return added;
+    }
+
+    public void setAdded(ArrayList<Integer> added) {
+        this.added = added;
+    }
 }
