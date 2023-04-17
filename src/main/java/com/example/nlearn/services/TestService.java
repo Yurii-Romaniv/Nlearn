df76871f1f0f05d6845217a585552884f3db2c83
Yurii Romaniv <arderchinsky@gmail.com>
2023-04-17 12:04:48 +0300

    @CrossOrigin transfered

--- src/main/java/com/example/nlearn/services/TestService.java
+++ src/main/java/com/example/nlearn/services/TestService.java
@@ -11,10 +11,12 @@ import com.example.nlearn.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @Service
+@CrossOrigin
 public class TestService {
 
     @Autowired
