90b4a97849c5e328c2df37c18fede3ed23b15565
Yurii Romaniv <arderchinsky@gmail.com>
2023-07-29 22:27:50 +0300

    added logout

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Nav, Navbar, NavbarBrand} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 function AppNavbar() {
     return <Navbar color="dark" dark expand="md">
-        <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+        <Nav> <NavbarBrand tag={Link} to="/">Home</NavbarBrand></Nav>
+        <Nav className="justify-content-end w-100"> <NavbarBrand
+            href={process.env.REACT_APP_SERVER_ENV + "/logout"}>Logout</NavbarBrand>
+        </Nav>
     </Navbar>;
 }
 
